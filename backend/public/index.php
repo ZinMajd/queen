78df8e899be55bbac602cd0e81ee5205a578e779
ZinMajd <https://github.com/ZinMajd/Scientific_conferences.git>
2026-04-21 01:08:58 +0300

    fix: remove redundant CORS headers to avoid browser conflict

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,10 +1,9 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept');
-
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept');
     exit;
 }
 
