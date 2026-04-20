9ec76c182c73fdd1d9d0cd1c9a286bd5a23c5417
ZinMajd <https://github.com/ZinMajd/Scientific_conferences.git>
2026-04-20 17:35:29 +0300

    Fix bootstrap/cache writable error on Vercel

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -20,4 +20,5 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create()
-    ->useStoragePath(env('APP_ENV') === 'production' ? '/tmp/storage' : null);
+    ->useStoragePath(env('APP_ENV') === 'production' ? '/tmp/storage' : null)
+    ->useBootstrapCachePath(env('APP_ENV') === 'production' ? '/tmp/bootstrap/cache' : null);
