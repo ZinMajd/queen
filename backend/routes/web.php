cf5d9d9dc1bd0bacf5ef5a83f368eebd08e8855f
ZinMajd <https://github.com/ZinMajd/Scientific_conferences.git>
2026-04-20 20:31:15 +0300

    Emergency: Move login route to web.php to bypass Vercel API routing issues

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -9,3 +9,9 @@ Route::get('/', function () {
         'status' => 'healthy'
     ]);
 });
+
+// Emergency routes in web.php to bypass API prefix issues
+Route::any('/api/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::get('/api/test', function() {
+    return response()->json(['message' => 'Web.php API test is working!']);
+});
